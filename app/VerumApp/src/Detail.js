e3c1e3fe4d3dab62a94fe7b7a2b39861d37e8485
ramirobg94 <ramiro.blazquezgon@alumnos.upm.es>
2017-03-11 06:05:49 +0100

    first View

--- app/VerumApp/src/Detail.js
+++ app/VerumApp/src/Detail.js
@@ -1,8 +1,4 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
+
 
 import React, { Component } from 'react';
 import {
@@ -12,13 +8,21 @@ import {
   View
 } from 'react-native';
 
-import Verum from './src/Verum';
+ class Detail extends Component {
+constructor(props){
+    super(props)
+    this.state = {
+      url:'',
+      likes:{}
+    }
+  } 
 
-export default class VerumApp extends Component {
   render() {
     return (
       <View style={styles.container}>
-        <Verum/>
+        
+      a
+       
       </View>
     );
   }
@@ -30,7 +34,17 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
     backgroundColor: '#F5FCFF',
-  }
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
 });
 
-AppRegistry.registerComponent('VerumApp', () => VerumApp);
+export default Detail;
